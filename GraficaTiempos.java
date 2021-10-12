b7a1f730490d33300642a7ee863021d9e8b09c68
user_kito <45264237+userkito@users.noreply.github.com>
2021-10-12 21:20:26 +0200

    graficas
    opcion de pintar mas de una grafica

---
+++ GraficaTiempos.java
@@ -0,0 +1,195 @@
+package p1;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GraficaTiempos extends JPanel {
+
+	private int width = 800;
+	private int heigth = 400;
+	private int padding = 25;
+	private int labelPadding = 25;
+	private Color lineColor = new Color(44, 102, 230, 180);
+	private Color pointColor = new Color(100, 100, 100, 180);
+	private Color gridColor = new Color(200, 200, 200, 200);
+	private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
+	private int pointWidth = 4;
+	private int numberYDivisions = 10;
+	private List<List<Integer>> puntos = new ArrayList<List<Integer>>();
+
+	public GraficaTiempos(List<Integer> scores) {
+		this.puntos.add(scores);
+	}
+
+	public GraficaTiempos() {
+	}
+
+	public void addList(List<Integer> scores) {
+		this.puntos.add(scores);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		for (List<Integer> scores : puntos) {
+			super.paintComponent(g);
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+			double xScale = ((double) getWidth() - (2 * padding) - labelPadding) / (scores.size() - 1);
+			double yScale = ((double) getHeight() - 2 * padding - labelPadding) / (getMaxScore() - getMinScore());
+
+			List<Point> graphPoints = new ArrayList<>();
+			for (int i = 0; i < scores.size(); i++) {
+				int x1 = (int) (i * xScale + padding + labelPadding);
+				int y1 = (int) ((getMaxScore() - scores.get(i)) * yScale + padding);
+				graphPoints.add(new Point(x1, y1));
+			}
+
+			// draw white background
+			g2.setColor(Color.WHITE);
+			g2.fillRect(padding + labelPadding, padding, getWidth() - (2 * padding) - labelPadding,
+					getHeight() - 2 * padding - labelPadding);
+			g2.setColor(Color.BLACK);
+
+			// create hatch marks and grid lines for y axis.
+			for (int i = 0; i < numberYDivisions + 1; i++) {
+				int x0 = padding + labelPadding;
+				int x1 = pointWidth + padding + labelPadding;
+				int y0 = getHeight() - ((i * (getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding
+						+ labelPadding);
+				int y1 = y0;
+				if (scores.size() > 0) {
+					g2.setColor(gridColor);
+					g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y1);
+					g2.setColor(Color.BLACK);
+					String yLabel = (int) ((int) ((getMinScore()
+							+ (getMaxScore() - getMinScore()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100 + "";
+					FontMetrics metrics = g2.getFontMetrics();
+					int labelWidth = metrics.stringWidth(yLabel);
+					g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
+				}
+				g2.drawLine(x0, y0, x1, y1);
+			}
+
+			// and for x axis
+			for (int i = 0; i < scores.size(); i++) {
+				if (scores.size() > 1) {
+					int x0 = i * (getWidth() - padding * 2 - labelPadding) / (scores.size() - 1) + padding
+							+ labelPadding;
+					int x1 = x0;
+					int y0 = getHeight() - padding - labelPadding;
+					int y1 = y0 - pointWidth;
+					if ((i % ((int) ((scores.size() / 20.0)) + 1)) == 0) {
+						g2.setColor(gridColor);
+						g2.drawLine(x0, getHeight() - padding - labelPadding - 1 - pointWidth, x1, padding);
+						g2.setColor(Color.BLACK);
+						String xLabel = i + "";
+						FontMetrics metrics = g2.getFontMetrics();
+						int labelWidth = metrics.stringWidth(xLabel);
+						g2.drawString(xLabel, x0 - labelWidth / 2, y0 + metrics.getHeight() + 3);
+					}
+					g2.drawLine(x0, y0, x1, y1);
+				}
+			}
+
+			// create x and y axes
+			g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, padding + labelPadding, padding);
+			g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, getWidth() - padding,
+					getHeight() - padding - labelPadding);
+
+			Stroke oldStroke = g2.getStroke();
+			g2.setColor(lineColor);
+			g2.setStroke(GRAPH_STROKE);
+			for (int i = 0; i < graphPoints.size() - 1; i++) {
+				int x1 = graphPoints.get(i).x;
+				int y1 = graphPoints.get(i).y;
+				int x2 = graphPoints.get(i + 1).x;
+				int y2 = graphPoints.get(i + 1).y;
+				g2.drawLine(x1, y1, x2, y2);
+			}
+
+			g2.setStroke(oldStroke);
+			g2.setColor(pointColor);
+			for (int i = 0; i < graphPoints.size(); i++) {
+				int x = graphPoints.get(i).x - pointWidth / 2;
+				int y = graphPoints.get(i).y - pointWidth / 2;
+				int ovalW = pointWidth;
+				int ovalH = pointWidth;
+				g2.fillOval(x, y, ovalW, ovalH);
+			}
+		}
+	}
+
+	// @Override
+//    public Dimension getPreferredSize() {
+//        return new Dimension(width, heigth);
+//    }
+	private double getMinScore() {
+		double minScore = Double.MAX_VALUE;
+		for (List<Integer> scores : puntos) {
+			for (Integer score : scores) {
+				minScore = Math.min(minScore, score);
+			}
+		}
+		return minScore;
+	}
+
+	private double getMaxScore() {
+		double maxScore = Double.MIN_VALUE;
+		for (List<Integer> scores : puntos) {
+			for (Integer score : scores) {
+				maxScore = Math.max(maxScore, score);
+			}
+		}
+
+		return maxScore;
+	}
+
+	public void setScores(List<Integer> scores) {
+		this.puntos = Collections.singletonList(scores);
+		invalidate();
+		this.repaint();
+	}
+
+	public void createAndShowGui() {
+		/**
+		 * TODO hacer que se puedan pintar hasta 4 graficas con distinto color (Se puede
+		 * hacer que reciba una lista de puntos y un color en el constructor puntos[0]
+		 * es la lista de Quicksort y puntos[1] es la lista de InsertionSort
+		 */
+		GraficaTiempos g1 = new GraficaTiempos(puntos.get(0));
+		GraficaTiempos g2 = new GraficaTiempos(puntos.get(1));
+		g1.setPreferredSize(new Dimension(500, 300));
+		g2.setPreferredSize(new Dimension(500, 300));
+		// tantos paneles como graficas queramos
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		// frame global
+		JFrame frame = new JFrame("Grafica de tiempos medios");
+		// layouy segun las graficas que haya
+		frame.setLayout(new GridLayout(1, 2));
+		// añadir paneles al frame
+		frame.add(panel1);
+		frame.add(panel2);
+		// añadir graficas a los paneles
+		panel1.add(g1);
+		panel2.add(g2);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
